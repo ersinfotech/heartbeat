320b4ede5f53c5378a4d1df5cc0a16dd661c5557
jarvisaoieong <kinua1230@gmail.com>
2017-12-01 17:02:09 +0800

    2.0

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 var ip = require('ip')
 var http = require('http')
+var https = require('https')
 var request = require('request-promise')
+var moment = require('moment')
+
+var lastRequestTime = moment().toISOString()
+var recordLastRequestTime = function (options) {
+  if (options.host !== 'heartbeat-api.ersinfotech.com') {
+    lastRequestTime = moment().toISOString()
+  }
+}
+
+var originHttpRequest = http.request
+http.request = function (options) {
+  recordLastRequestTime(options)
+  return originHttpRequest.apply(this, arguments)
+}
+var originHttpsRequest = https.request
+https.request = function (options) {
+  recordLastRequestTime(options)
+  return originHttpsRequest.apply(this, arguments)
+}
 
 var data = {
   ip: ip.address(),
@@ -12,22 +32,24 @@ var interval = 0
 
 module.exports = function() {
   setTimeout(function() {
-    request({
-      method: 'post',
-      url: 'http://heartbeat-api.ersinfotech.com/graphql',
-      json: true,
-      body: {
-        query: "mutation ping ($ip: String! $path: String! $type: String) {ping(ip: $ip path: $path type: $type)}",
-        variables: {
-          ip: data.ip,
-          path: data.path,
-          type: type,
+    if (moment().diff(lastRequestTime, 'minutes', true) < 10) {
+      request({
+        method: 'post',
+        url: 'http://heartbeat-api.ersinfotech.com/graphql',
+        json: true,
+        body: {
+          query: "mutation ping ($ip: String! $path: String! $type: String) {ping(ip: $ip path: $path type: $type)}",
+          variables: {
+            ip: data.ip,
+            path: data.path,
+            type: type,
+          },
         },
-      },
-    })
-    .catch(function(err) {
-      console.error(err.message)
-    })
+      })
+      .catch(function(err) {
+        console.error(err.message)
+      })
+    }
     type = 'live'
     interval = 30
     module.exports()
